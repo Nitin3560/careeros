4c0593735f9b979237d227fb3f1d8f6a2bdffe0b
Nitin singh Rathore <nxr3560@mavs.uta.edu>
2026-09-12 02:13:33 -0500

    Rename tailoring migration revision

--- apps/api/alembic/versions/e4f1a2b3c9d8_add_tailoring_pipeline.py
+++ apps/api/alembic/versions/e4f1a2b3c9d8_add_tailoring_pipeline.py
@@ -1,6 +1,6 @@
 """add tailoring pipeline tables
 
-Revision ID: b410cd4
+Revision ID: e4f1a2b3c9d8
 Revises: c9d0e1f2a3b4
 Create Date: 2026-09-12 00:00:00.000000
 
@@ -11,7 +11,7 @@ from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
-revision: str = "b410cd4"
+revision: str = "e4f1a2b3c9d8"
 down_revision: Union[str, Sequence[str], None] = "c9d0e1f2a3b4"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
